45889130d58e41aaa41c88da215e4c1573d8e4b2
William Lin <wlin26@yahoo.com>
2016-10-28 21:13:45 -0400

    Created a link to go to top
    Created a link that appears after the nav bar disappears on scroll. The
    user will be able to click this link and return to the top of the site.

--- Resume/js/script.js
+++ Resume/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $(".toTop").hide();
 	$('a').click(function(){
     	$('html, body').animate({
         	scrollTop: $( $.attr(this, 'href') ).offset().top
@@ -11,7 +12,9 @@ $(window).scroll(function(){
     var currentTop = $(window).scrollTop();
     if (currentTop < 500) {
         $(".nav").show();
+        $(".toTop").hide();
     } else{
         $(".nav").hide();
+        $(".toTop").show();
     }
 });
