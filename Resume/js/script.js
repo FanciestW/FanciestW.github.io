8bfe00f4ef269b63ebccd5e4ded2119a3bd26f0f
William Lin <wlin26@yahoo.com>
2016-11-29 16:09:38 -0500

    Updated site to have better looks and feel

--- Resume/js/script.js
+++ Resume/js/script.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
-    $(".toTop").hide();
+    $('.toTop').hide();
 	$('a').click(function(){
     	$('html, body').animate({
         	scrollTop: $( $.attr(this, 'href') ).offset().top
     	}, 500);
     	return false;
 	});
-    $(".scrollspy").scrollspy();
 });
 
 $(window).scroll(function(){
     var currentTop = $(window).scrollTop();
     if (currentTop < 500) {
-        $(".navbar-fixed").show();
-        $(".toTop").hide();
-    } else{
-        $(".navbar-fixed").hide();
-        $(".toTop").show();
+        $(".navbar-fixed").fadeIn(300);
+        $(".toTop").fadeOut(300);
+    } else {
+        $(".navbar-fixed").fadeOut(300);
+        $(".toTop").fadeIn(300);
     }
 });
