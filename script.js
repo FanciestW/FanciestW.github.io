753114854488519afa90d0a2ce53269d6666ea79
William Lin <wlin26@yahoo.com>
2016-12-23 23:09:47 -0500

    Added FIrebase to receive mesages

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+	// Initialize Firebase
+  	var config = {
+	    apiKey: "AIzaSyB9WoQ_uYkFmUFB1c_6ihLwl8jDCQPRJtM",
+	    authDomain: "contactme-313af.firebaseapp.com",
+	    databaseURL: "https://contactme-313af.firebaseio.com",
+	    storageBucket: "contactme-313af.appspot.com",
+	    messagingSenderId: "1096291680692"
+  	};
+  	firebase.initializeApp(config);
+
 	$('.parallax').parallax();
 	$('.modal').modal();
 });
@@ -9,4 +19,20 @@ function clearForm(){
 	$('#email').val("");
 	$('#message').val("");
 	console.log("Tested");
+}
+
+function sendMsg(){
+	var database = firebase.database().ref('messages');
+	var newMsg = database.push();
+	var fName = document.getElementById('first_name').value;
+	var lName = document.getElementById('last_name').value;
+	var em = document.getElementById('email').value;
+	var msg = document.getElementById('message').value;
+	newMsg.set({
+		firstName: fName,
+		lastName: lName,
+		email: em,
+		message: msg
+	});
+	clearForm();
 }
