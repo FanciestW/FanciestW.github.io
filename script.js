b7194cc42a925138a59036ba7a2e077a7068a9d2
William Lin <wlin26@yahoo.com>
2016-12-23 22:43:11 -0500

    Added a Modal with a form
    A modal with a contact form inside to send message to me

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 	$('.parallax').parallax();
+	$('.modal').modal();
 });
+
+function clearForm(){
+	$('#first_name').val("");
+	$('#last_name').val("");
+	$('#email').val("");
+	$('#message').val("");
+	console.log("Tested");
+}
